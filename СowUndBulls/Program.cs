24ecdaaad507928b94335efebb0ae965e9f7f310
157000 <157000@DESKTOP-LQOD1JV>
2022-09-26 16:34:15 +0500

    Добавьте файлы проекта.

---
+++ СowUndBulls/Program.cs
@@ -0,0 +1,34 @@
+﻿// See https://aka.ms/new-console-template for more information
+while (true)
+{
+    Console.WriteLine("Введите четырехзначное число");
+    while (true)
+    {
+        try
+            {
+            int number = Convert.ToInt32(Console.ReadLine());
+            if (number < 1000 || number > 9999)
+            {
+                Console.WriteLine("Ошибка, введите четырехзначное число");
+                break;
+            }
+            int num1 = number / 1000;
+            int num2 = (number % 1000) / 100;
+            int num3 = (number % 100) / 10;
+            int num4 = number %10;
+            Console.WriteLine(num1 + " " + num2 + " " + num3+" "+num4);
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine("Ошибка, введите цифровое значение");
+        }
+        Random random = new Random();
+        int randNumber = random.Next(0,1000);
+    } 
+     
+    
+}
+public class rand
+{
+    
+}
